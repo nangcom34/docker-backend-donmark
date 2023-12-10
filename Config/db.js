088aadff54df134ba52f1aa95509f37a79314fe0
nangcom34 <worktoittisak38@gmail.com>
2023-12-10 11:30:43 +0700

    first commit1

--- Config/db.js
+++ Config/db.js
@@ -1,13 +1,16 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.DATABASE)
-        console.log(`ConnectDB Success`)
+        await mongoose.connect(process.env.DATABASE, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log(`ConnectDB Success`);
     } catch (error) {
-        console.log(error)
-        process.exit(1)
+        console.error(`Error connecting to the database: ${error.message}`);
+        process.exit(1);
     }
-}
+};
 
-module.exports = connectDB
+module.exports = connectDB;
